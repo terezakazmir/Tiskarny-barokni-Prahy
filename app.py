0d05de0b358bae7e39ae917fe464a8447369e8d0
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-09-26 22:13:49 +0200

    arguments for port, host and debug

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
+import argparse
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], use_pages=True)
 
@@ -38,5 +39,17 @@ def redirect_to_default(pathname):
     return dash.no_update
 
 
+parser = argparse.ArgumentParser(
+    description="Run the Dash app",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument("--port", type=int, default=8050, help="Port to run the app on")
+parser.add_argument(
+    "--host", type=str, default="0.0.0.0", help="Host to run the app on"
+)
+parser.add_argument("--debug", action="store_true", help="Run the app in debug mode")
+
+args = parser.parse_args()
+
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run(host=args.host, port=str(args.port), debug=args.debug)
