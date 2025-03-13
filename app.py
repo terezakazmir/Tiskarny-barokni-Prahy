b16ac2d396abf7a7e5b451ae99cfd2c3e9d642c7
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-03-13 20:24:06 +0100

    added waitress

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 import argparse
+import waitress
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], use_pages=True)
 
@@ -47,9 +48,9 @@ parser.add_argument("--port", type=int, default=8050, help="Port to run the app 
 parser.add_argument(
     "--host", type=str, default="0.0.0.0", help="Host to run the app on"
 )
-parser.add_argument("--debug", action="store_true", help="Run the app in debug mode")
+parser.add_argument("--threads", type=int, help="Number of threads to use")
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    app.run(host=args.host, port=str(args.port), debug=args.debug)
+    waitress.serve(app.server, host=args.host, port=args.port, threads=args.threads)
